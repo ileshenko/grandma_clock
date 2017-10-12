516f1d2ee885b2e8a0b74303030eda4da02f5709
Andrey Leshenko <andrey.leshenko@gmail.com>
2017-10-12 18:50:18 +0300

    UTF-8 Messages

--- messages.js
+++ messages.js
@@ -1,4 +1,5 @@
-var messages = [
-	["2017-10-12", "Reminder: The Hunger Games start tomorrow!"],
-	["2017-10-13", "What did the fox say?"],
+﻿var messages = [
+	["2017-10-14", "What did the fox say?"],
+	["2017-10-13", "Reminder: The Hunger Games start tomorrow!"],
+	["2017-10-12", "Бережёного Бог бережёт."],
 ];
